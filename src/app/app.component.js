57a9a7da8ab89fc4d199834f138327c2db40e27e
Ty Smith <dts5004@gmail.com>
2018-02-11 15:57:32 -0800

    Added app module and componet

---
+++ src/app/app.component.js
@@ -0,0 +1,10 @@
+(function(){
+    "use strict";
+
+    angular
+        .module("app")
+        .component("tsApp", {
+            controller: function(){},
+            template: "<h1>hello world</h1>"
+        });
+})();
